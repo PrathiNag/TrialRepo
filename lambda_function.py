cc1668d7349e3809dfdf7c1fb3c2a565d6ee7cd5
PrathiNag <prathi_nag@outlook.com>
2018-04-23 15:53:39 +0530

    dynamodb

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,12 @@
+import boto3
+dynamodb = boto3.client('dynamodb');
+
+
 def lambda_handler(event, context):
-    
-	print ("HI UNIVERSE");
+    response= dynamodb.create_table(
+    AttributeDefinitions=[{ 'AttributeName' : 'string', 'AttributeType' : 'S'}],
+    TableName='string',
+    KeySchema=[{'AttributeName' : 'string','KeyType' : 'HASH'}],
+    ProvisionedThroughput={'ReadCapacityUnits':1, 'WriteCapacityUnits':2}
+    );
+    print(response);
